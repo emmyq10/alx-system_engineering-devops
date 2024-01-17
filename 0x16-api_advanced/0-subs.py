191defed4c87a4ae455ede693cf3e36bca945d9e
emmyq10 <emmatunde2001@gmail.com>
2024-01-16 18:49:20 -0800

    task completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""0-subs module"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Queries the Reddit API
+
+    Returns:
+        the number of subscribers for a given subreddit,
+        0 if an invalid subreddit is given
+    """
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/about.json'.
+                       format(URL, subreddit),
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
+    if res.status_code != 200:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs)
+    return res.json().get('data').get('subscribers')
